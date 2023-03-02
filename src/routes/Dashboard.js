026e8ebc8b4958f121fc7186b24f2d5f323d98ef
david0github <david.nguyen@quinnipiac.edu>
2023-03-01 21:18:53 -0500

    some more styling

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -8,21 +8,23 @@ import TargetAndActual from "../components/charts/TargetAndActual.js";
 import AverageScrap from "../components/charts/AverageScrap.js";
 import AverageDowntime from "../components/charts/AverageDowntime.js";
 import GoodVsScrap from "../components/charts/GoodVsScrap.js";
+import "./dashboard.css";
+
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
       <Container fluid>
       <Row>
-        <Col><h6>Finished vs Rework vs Scrap Products</h6> <PieChart /></Col>
-        <Col><h6>Number of Finished Products</h6><FinishedProduct/>
+        <Col className="graphs"><h6>Finished vs Rework vs Scrap Products</h6> <PieChart /></Col>
+        <Col className="graphs"><h6>Number of Finished Products</h6><FinishedProduct/>
         </Col>
-        <Col> <h6>Target vs Actual Products</h6><TargetAndActual/></Col>
+        <Col className="graphs"> <h6>Target vs Actual Products</h6><TargetAndActual/></Col>
       </Row>
       <Row>
-        <Col> <h6>Average Amount of Scrap by Cell</h6><AverageScrap/></Col>
-        <Col><h6>Average Amount of Downtime by Cell</h6><AverageDowntime/></Col>
-        <Col> <h6>Good vs Scrap Product</h6><GoodVsScrap/></Col>
+        <Col className="graphs"> <h6>Average Amount of Scrap by Cell</h6><AverageScrap/></Col>
+        <Col className="graphs"><h6>Average Amount of Downtime by Cell</h6><AverageDowntime/></Col>
+        <Col className="graphs"> <h6>Good vs Scrap Product</h6><GoodVsScrap/></Col>
       </Row>
         
       </Container>
